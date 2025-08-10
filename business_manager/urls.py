6497b0146fcd033c33a3a766438fecc8f9a73d5a
Niyonzima123 <niyonzimaisaac89@gmail.com>
2025-08-10 06:48:36 -0700

    feat: Finalize project configuration and resolve media/static file issues level1

--- business_manager/urls.py
+++ business_manager/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-
-# business_manager/urls.py
+ # business_manager/urls.py
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve # ADD THIS IMPORT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,24 +29,13 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-    # business_manager/urls.py
-
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings  # <-- Make sure this is here
-from django.conf.urls.static import static  # <-- And this
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('inventory.urls')),
-    path('accounts/', include('accounts.urls')),
+# This is for serving media files on Render (production)
+# The DEBUG block below is for local development only.
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
-# This block serves media files ONLY when DEBUG is True
+
+# This block serves media files ONLY when DEBUG is True (local development)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
